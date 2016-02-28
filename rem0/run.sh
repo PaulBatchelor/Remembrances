b39bfcf9997b600f879928c162caa57c827e4ba9
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-02-28 12:18:20 +0000

    added gitignore

--- rem0/run.sh
+++ rem0/run.sh
@@ -1,4 +1,4 @@
-alias jacksporth=~/sporth/util/jacksporth
+alias jsporth=~/remembrances/bin/jsporth
 ps cax | grep  jackd | grep -v grep > /dev/null
 
 if [ $? -eq 0 ]
@@ -8,7 +8,7 @@ else
 	echo "jackd is not running. starting..."
 	cd ~/remembrances/rem0
 	./run_jack &
-	jacksporth test.sp
+	jsporth test.sp
 fi
 
 
